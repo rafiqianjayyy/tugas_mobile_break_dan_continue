be9f09182f22be0ae88be2430c2e34b7e152547f
rafiqi <muhammadrafiqi171@gmail.com>
2024-10-12 22:25:00 +0700

    Create break.dart

---
+++ break.dart
@@ -0,0 +1,24 @@
+//INI ADALAH SALAH SATU CONTOH PENGGUNAAN BREAK
+// void main(){
+//   var jumlah = 1;
+
+//   while(true){
+//     print('sudah di ulang sebanyak = -$jumlah');
+//     jumlah++;
+
+//     if(jumlah > 10){
+//       break;
+//     }
+//   }
+// }
+
+//INI ADALAH CONTOH PENGGUNAAN CONTINUE
+// void main(){
+//   for(var jumlah = 1;jumlah <=100;jumlah++){
+//   if(jumlah % 2 == 0){
+//     continue;
+//     }
+
+//     print('bilangan ganjil -$jumlah');
+//   }
+// }
